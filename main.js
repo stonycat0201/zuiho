2139aa3351ac0db8228a5f0a608054bb5479d0b5
stonycat <39594753+stonycat0201@users.noreply.github.com>
2022-08-23 05:24:26 +0700

    Add project files.

---
+++ main.js
@@ -0,0 +1,20 @@
+require('dotenv').config;
+
+const { Client, Intents } = require('discord.js');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+
+client.on('ready', () => {
+    console.log(`logged in as ${client.user.tag}!`);
+});
+
+client.on("message", msg => {
+  if (msg.author.bot) return
+    
+  if (msg.content === "$inspire") {
+    getQuote().then(quote => msg.channel.send(quote))
+  }
+})
+
+client.login(process.env.CLIENT_TOKEN); //login
